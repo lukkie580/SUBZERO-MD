f06f656a4ab14e28b481c775bb0f74da4c6e4bc1
lukkie580 <jonc454588@gmail.com>
2025-10-07 17:39:11 +0100

    Update settings.js for Lukkie580

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~24c7c13972d9e6eac4307ec9723c444960da166a",
+  OWNER_NUMBER: "+2348140074277", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
